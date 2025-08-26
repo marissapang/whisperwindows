671c22fea7293f0b56bb0daf693003b3165ce40a
Marissa <1995marissapsy@live.com>
2025-08-26 11:44:51 -0400

    missing import fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Suspense } from 'react';
 import { GoogleTagManager } from '@next/third-parties/google';
 import GtmPageView from './gtm-pageview'; //
 
